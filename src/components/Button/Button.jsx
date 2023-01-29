aef5d6d5be6e11ffa119daa97feca5279fd25c3a
Ashish Keshri <keshri.ashish1@gmail.com>
2023-01-29 19:09:27 +0530

    buttons

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import './button.styles.scss';
+import React from "react";
+import "./button.styles.scss";
 
-const Button = () => {
+const BUTTON_TYPE_CLASSES = {
+  google:'google',
+  facebook: "facebook",
+  github: "github",
+  signup: "signup",
+};
+
+const Button = ({ children, buttonType, ...otherProps }) => {
   return (
-    <button>Button</button>
-  )
-}
+    <button className={`${BUTTON_TYPE_CLASSES[buttonType]}`} {...otherProps}>
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
